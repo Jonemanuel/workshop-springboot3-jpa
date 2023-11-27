be145fee05f7f2690f22a3b3f6eda3bfd56ade42
Jônatas Emanuel <jonatasemanuel276@gmail.com>
2023-11-27 01:27:18 -0300

    Payment, one-to-one association

--- src/main/java/com/jonweb/course/config/TestConfig.java
+++ src/main/java/com/jonweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jonweb.course.entities.Category;
 import com.jonweb.course.entities.Order;
 import com.jonweb.course.entities.OrderItem;
+import com.jonweb.course.entities.Payment;
 import com.jonweb.course.entities.Product;
 import com.jonweb.course.entities.User;
 import com.jonweb.course.entities.enums.OrderStatus;
@@ -77,5 +78,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2 , oi3, oi4 ));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		
+		orderRepository.save(o1);
 	}
 }
