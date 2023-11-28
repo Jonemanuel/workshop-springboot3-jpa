e21537d346a1ac4fa9ece4a31cc28e96a7189131
Jônatas Emanuel <jonatasemanuel276@gmail.com>
2023-11-28 09:02:09 -0300

    Exception handling - update

--- src/main/java/com/jonweb/course/services/UserService.java
+++ src/main/java/com/jonweb/course/services/UserService.java
@@ -13,46 +13,54 @@ import com.jonweb.course.repositories.UserRepository;
 import com.jonweb.course.services.exceptions.DatabaseException;
 import com.jonweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findALL(){
+
+	public List<User> findALL() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		repository.findById(id);
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+
 	public User insert(User obj) {
 		return repository.save(obj);
-		
+
 	}
-	
-	public void delete(Long id ) {
+
+	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-}
-		
-	 public User update (Long id, User obj) {
-		 	User entity = repository.getReferenceById(id);
-		 	updateData(entity, obj); 
-		 	return repository.save(entity);
-	 }
+	}
+
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	}
+}
